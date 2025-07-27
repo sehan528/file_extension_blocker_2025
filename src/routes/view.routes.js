ca50cd78ad8d3f9d8fc6d7029ec48e5b20d87ce0
Sechang Jang <71550827+sehan528@users.noreply.github.com>
2025-07-27 15:12:21 +0900

    feat: 메인 페이지 및 로그인 페이지 라우트

--- src/routes/view.routes.js
+++ src/routes/view.routes.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const viewController = require('../controllers/view.controller');
 
-console.log('🌐 Index routes 로드 완료');
+console.log('🌐 View routes 로드 완료 (인증 페이지 포함)');
 
-// 메인 페이지 라우트
+// 메인 페이지 (인증 필요)
 router.get('/', viewController.serveHomePage);
 
-// 로그인 페이지 라우트들
+
+// 인증 관련 페이지 (공개)
+
+// 로그인 페이지
 router.get('/login.html', (req, res) => {
-    const path = require('path');
     res.sendFile(path.join(__dirname, '../../public', 'login.html'));
 });
 
+// 로그인 페이지 (확장자 없는 경로)
 router.get('/login', (req, res) => {
-    const path = require('path');
     res.sendFile(path.join(__dirname, '../../public', 'login.html'));
 });
 
